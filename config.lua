61fabc8d1bcd8b5c1abd32eeb46b19dc47133c64
Alex Davis <alx@juxt.pro>
2023-02-28 10:18:06 +0000

    neovide

--- config.lua
+++ config.lua
@@ -12,3 +12,16 @@ require("armincerf.commands")
 require("armincerf.keymaps")
 
 require("armincerf.lsp")
+
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_trail_size = 0
+  vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('', '<D-s>', ':update<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('!', '<D-s>', ':update<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('t', '<D-s>', ':update<CR>', { noremap = true, silent = true })
+end
